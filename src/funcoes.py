402cb5e44b39980a297b911190e486d1d760643e
Igor Falco <igor.falco@dtidigital.com.br>
2025-04-03 11:40:16 -0300

    Geração do grafo

--- src/funcoes.py
+++ src/funcoes.py
@@ -9,14 +9,13 @@ import random
 def verificaSolvabilidade(tabuleiro):
     inversoes = 0
     posicaoVazia = -1
-    print(tabuleiro)
-
-    for i in range(15): # Percorre até o penúltimo elemento
-        for j in range(i+1, 16): # Compara com os elementos à frente
-            
-            if tabuleiro[i] > tabuleiro[j] and tabuleiro[i] != 0 and tabuleiro[j] != 0:
-                inversoes+= 1
     
+    for i in range(15):  # Percorre até o penúltimo elemento
+        for j in range(i+1, 16):  # Compara com os elementos à frente
+
+            if tabuleiro[i] > tabuleiro[j] and tabuleiro[i] != 0 and tabuleiro[j] != 0:
+                inversoes += 1
+
     # Encontra a posição do número 0 (espaço vazio)
     posicaoVazia = tabuleiro.index(0)
 
@@ -24,12 +23,13 @@ def verificaSolvabilidade(tabuleiro):
         linhaVazia = posicaoVazia//4  # Linha em que está o espaço vazio
 
     # Verifica se é solucionável
-    if (inversoes%2 == 0 and linhaVazia%2 != 0) or (inversoes%2 != 0 and linhaVazia%2==0):
+    if (inversoes % 2 == 0 and linhaVazia % 2 != 0) or (inversoes % 2 != 0 and linhaVazia % 2 == 0):
         return True
     else:
         return False
-    
+
 #########   Tarefa 2   #########
+
 
 def gerarTabuleiro():
     tabuleiro = list(range(16))  # Cria uma lista [0, 1, 2, ..., 15]
@@ -37,5 +37,3 @@ def gerarTabuleiro():
     return tabuleiro
 
 #########   Tarefa 3   #########
-
-
